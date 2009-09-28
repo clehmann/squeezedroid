bd174fc6690b07a355d322e4c3f9e2be94bcd7c4
lehmannc <lehmannc@ce0f0c34-966b-11de-9a8c-5f207592b8ba>
2009-09-28 00:31:26 +0000

    Correctly decode url-type encoding from squeezeserver.
    git-svn-id: https://squeeze-droid.googlecode.com/svn/trunk@36 ce0f0c34-966b-11de-9a8c-5f207592b8ba

--- src/net/chrislehmann/util/SerializationUtils.java
+++ src/net/chrislehmann/util/SerializationUtils.java
@@ -5,61 +5,112 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang.StringUtils;
-
 public class SerializationUtils
 {
-   public static <T> List<T> unserializeList( Pattern pattern, String input, Unserializer<T> unserializer )
+   public static <T> List<T> unserializeList(Pattern pattern, String input, Unserializer<T> unserializer)
    {
       List<T> items = new ArrayList<T>();
-      
+
       Matcher matcher = pattern.matcher( input );
 
-      while( matcher.find() )
+      while ( matcher.find() )
       {
          items.add( unserializer.unserialize( matcher ) );
       }
-      
+
       return items;
    }
 
-   public static <T> T unserialize( Pattern pattern, String input, Unserializer<T> unserializer )
+   public static <T> T unserialize(Pattern pattern, String input, Unserializer<T> unserializer)
    {
       T object = null;
       Matcher matcher = pattern.matcher( input );
-      if( matcher.find() )
+      if ( matcher.find() )
       {
          object = unserializer.unserialize( matcher );
       }
-      
+
       return object;
    }
-   
 
-   public static String decode(String string)
+
+   /**
+    * Lifted from http://www.w3.org/International/O-URL-code.html
+    * @param s String to decode
+    * @return a decoded string
+    */
+   public static String decode(String s)
    {
-      
-      String decodedString = null;
-      if( string != null  )
+      StringBuffer sbuf = new StringBuffer();
+      if ( s != null )
       {
-         decodedString = string.replace( "%20", " " );
-         decodedString = decodedString.replace( "%2C", "," );
-         decodedString = decodedString.replace( "%26", "&" );
-         decodedString = decodedString.replace( "%3A", ":" );
-         decodedString = decodedString.replace( "%24", "$" );
-         decodedString = decodedString.replace( "%26", "&" );
-         decodedString = decodedString.replace( "%2B", "+" );
-         decodedString = decodedString.replace( "%2F", "/" );
-         decodedString = decodedString.replace( "%3A", ":" );
-         decodedString = decodedString.replace( "%3B", ";" );
-         decodedString = decodedString.replace( "%3F", "?" );
-         decodedString = decodedString.replace( "%40", "@" );
+
+         int l = s.length();
+         int ch = -1;
+         int b, sumb = 0;
+         for ( int i = 0, more = -1; i < l; i++ )
+         {
+            /* Get next byte b from URL segment s */
+            switch ( ch = s.charAt( i ) )
+            {
+               case '%' :
+                  ch = s.charAt( ++i );
+                  int hb = (Character.isDigit( (char) ch ) ? ch - '0' : 10 + Character.toLowerCase( (char) ch ) - 'a') & 0xF;
+                  ch = s.charAt( ++i );
+                  int lb = (Character.isDigit( (char) ch ) ? ch - '0' : 10 + Character.toLowerCase( (char) ch ) - 'a') & 0xF;
+                  b = (hb << 4) | lb;
+                  break;
+               case '+' :
+                  b = ' ';
+                  break;
+               default :
+                  b = ch;
+            }
+            /* Decode byte b as UTF-8, sumb collects incomplete chars */
+            if ( (b & 0xc0) == 0x80 )
+            { // 10xxxxxx (continuation byte)
+               sumb = (sumb << 6) | (b & 0x3f); // Add 6 bits to sumb
+               if ( --more == 0 )
+                  sbuf.append( (char) sumb ); // Add char to sbuf
+            }
+            else if ( (b & 0x80) == 0x00 )
+            { // 0xxxxxxx (yields 7 bits)
+               sbuf.append( (char) b ); // Store in sbuf
+            }
+            else if ( (b & 0xe0) == 0xc0 )
+            { // 110xxxxx (yields 5 bits)
+               sumb = b & 0x1f;
+               more = 1; // Expect 1 more byte
+            }
+            else if ( (b & 0xf0) == 0xe0 )
+            { // 1110xxxx (yields 4 bits)
+               sumb = b & 0x0f;
+               more = 2; // Expect 2 more bytes
+            }
+            else if ( (b & 0xf8) == 0xf0 )
+            { // 11110xxx (yields 3 bits)
+               sumb = b & 0x07;
+               more = 3; // Expect 3 more bytes
+            }
+            else if ( (b & 0xfc) == 0xf8 )
+            { // 111110xx (yields 2 bits)
+               sumb = b & 0x03;
+               more = 4; // Expect 4 more bytes
+            }
+            else
+            /*if ((b & 0xfe) == 0xfc)*/{ // 1111110x (yields 1 bit)
+               sumb = b & 0x01;
+               more = 5; // Expect 5 more bytes
+            }
+            /* We don't test if the UTF-8 encoding is well-formed */
+         }
       }
-      return decodedString;
+
+      return sbuf.toString();
    }
 
    public interface Unserializer<T>
    {
-      T unserialize( Matcher matcher );
+      T unserialize(Matcher matcher);
    }
 }
